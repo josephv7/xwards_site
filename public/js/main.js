b46353b89a51e1f58600388e8f131ca1fad18c3c
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-11-26 18:53:54 +0530

    added logos to site

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,31 @@
 (function($) {
 	"use strict"
 
+// For client scroller
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
 	///////////////////////////
 	// Preloader
 	$(window).on('load', function() {
