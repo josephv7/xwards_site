21dc6171122993d2cc8d2178e11c9b208390ab0c
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-11-26 19:45:31 +0530

    changed client list call from js file to script tag

--- public/js/main.js
+++ public/js/main.js
@@ -2,28 +2,28 @@
 	"use strict"
 
 // For client scroller
-$(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 8,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
+// $(document).ready(function(){
+//     $('.customer-logos').slick({
+//         slidesToShow: 8,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 1500,
+//         arrows: false,
+//         dots: false,
+//         pauseOnHover: false,
+//         responsive: [{
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 4
+//             }
+//         }, {
+//             breakpoint: 520,
+//             settings: {
+//                 slidesToShow: 3
+//             }
+//         }]
+//     });
+// });
 
 
 	///////////////////////////
