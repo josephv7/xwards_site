6ff70907d1ef2c7060a3b3e934848dff0a9a99f1
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-11-27 15:23:37 +0530

    changed testimonial and about to slick, code for own commented

--- public/js/main.js
+++ public/js/main.js
@@ -90,21 +90,21 @@
 	// Owl Carousel
 
 
-	$('#testimonial-slider').owlCarousel({
-		loop:true,
-		margin:15,
-		dots : true,
-		nav: false,
-		autoplay : true,
-		responsive:{
-			0: {
-				items:1
-			},
-			992:{
-				items:2
-			}
-		}
-	});
+	// $('#testimonial-slider').owlCarousel({
+	// 	loop:true,
+	// 	margin:15,
+	// 	dots : true,
+	// 	nav: false,
+	// 	autoplay : true,
+	// 	responsive:{
+	// 		0: {
+	// 			items:1
+	// 		},
+	// 		992:{
+	// 			items:2
+	// 		}
+	// 	}
+	// });
 
 
 	// TODO commented to resolve owl carousel error
@@ -119,27 +119,27 @@
 	// 	animateOut: 'fadeOut'
 	// });
 
-	$('#about-slider2').owlCarousel({
-		items:1,
-		loop:true,
-		margin:15,
-		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		dots : true,
-		autoplay : true,
-		animateOut: 'fadeOut'
-	});
+	// $('#about-slider2').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	margin:15,
+	// 	nav: true,
+	// 	navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	// 	dots : true,
+	// 	autoplay : true,
+	// 	animateOut: 'fadeOut'
+	// });
 
-	$('#about-slider3').owlCarousel({
-		items:1,
-		loop:true,
-		margin:15,
-		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		dots : true,
-		autoplay : true,
-		animateOut: 'fadeOut'
-	});
+	// $('#about-slider3').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	margin:15,
+	// 	nav: true,
+	// 	navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	// 	dots : true,
+	// 	autoplay : true,
+	// 	animateOut: 'fadeOut'
+	// });
 
 	
 
